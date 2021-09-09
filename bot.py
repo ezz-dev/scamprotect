04da401252c78d9eb431a945808a4e7ca782419f
xshadowsexy <82493057+Sweety187@users.noreply.github.com>
2021-09-09 19:11:17 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ bot = commands.Bot(command_prefix='!', intents=discord.Intents().all(),
 Token = "TOKEN"
 embed_blacklist = ["discord nitro бесплатно на 3 месяца от steam", "сделайте discord ещё круче с nitro",
 		   "3 months of discord nitro free from steam", "get 3 months of discord nitro free from steam"]
-patterns_blacklist = [r"i'm leaving.*skin.*https:\/\/", r"i'm leaving.*skin.*http:\/\/", r"i'm leaving.*inventory.*http"]
+patterns_blacklist = [r"i'm leaving.*skin.*http", r"i'm leaving.*inventory.*http"]
 reasons = ["blacklist.link: {}", "blacklist.embed: {}", "blacklist.pattern: {}"]
 
 with open("blacklist.txt") as file:
@@ -16,7 +16,8 @@ with open("blacklist.txt") as file:
 
 @bot.command()
 async def invite(ctx):
-	embed = discord.Embed(description=f":small_blue_diamond: Add me to your server: [[Invite]](https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions=8&scope=bot)")
+	link = f"https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions=8&scope=bot"
+	embed = discord.Embed(description=f":small_blue_diamond: Add me to your server: [[Invite]]({link})")
 	await ctx.send(embed=embed)
 
 
@@ -34,14 +35,18 @@ async def on_message(message):
 	await bot.process_commands(message)
 	index = 0
 	for elem in blacklist:
+		# Scan message for blacklisted links
 		if elem in message.content and elem != "":
 			return await delete(message, index, 0, 0, "message.content")
 		index += 1
 	index = 0
 	if not message.embeds and "http" in message.content:
+		# If message contains a link but doesn't have an embed,
+		# fetch this message again to get loaded (probably) embed
 		await asyncio.sleep(1)
 		message = await message.channel.fetch_message(message.id)
 	for elem in embed_blacklist:
+		# Scan titles of all embeds in message
 		indexx = 0
 		for embed in message.embeds:
 			if elem in embed.title.lower() and elem != "":
@@ -50,6 +55,7 @@ async def on_message(message):
 		index += 1
 	index = 0
 	for elem in embed_blacklist:
+		# Scan descriptions of all embeds in message
 		indexx = 0
 		for embed in message.embeds:
 			if elem in embed.description.lower() and elem != "":
@@ -58,6 +64,7 @@ async def on_message(message):
 		index += 1
 	index = 0
 	for elem in patterns_blacklist:
+		# Check message for suspicious pattern
 		if re.findall(elem, message.content.lower()):
 			return await delete(message, index, 0, 2, "message.content")
 		index += 1
