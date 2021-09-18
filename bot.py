4698cf315828046c9f229fa52ab48b0d277805f9
xshadowsexy <82493057+Sweety187@users.noreply.github.com>
2021-09-18 23:17:49 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,90 +1,70 @@
-import discord, asyncio, re
+import discord
 from discord.ext import commands
+from ezlib import *
 
-bot = commands.Bot(command_prefix='!', intents=discord.Intents().all(),
-		   case_insensitive=True, help_command=None)
+bot = commands.Bot(command_prefix=determine_prefix,
+		   intents=discord.Intents().all(),
+		   case_insensitive=True,
+		   help_command=None)
 Token = "TOKEN"
-embed_blacklist = ["discord nitro бесплатно на 3 месяца от steam", "сделайте discord ещё круче с nitro",
-		   "3 months of discord nitro free from steam", "get 3 months of discord nitro free from steam",
-		   "discord nitro for 3 months with steam", "free discord nitro for 3 months from steam",
-		   "make discord even cooler with nitro"]
-patterns_blacklist = [r"i'm leaving.*skin.*http", r"i'm leaving.*inventory.*http",
-                      r"i am leaving.*trade.*http", r"i leave.*trade.*http",
-		      r"@everyone.*nitro.*free.*steam.*http"]
-reasons = ["blacklist.link: {}", "blacklist.embed: {}", "blacklist.pattern: {}"]
-
-with open("blacklist.txt") as file:
-	_text_ = file.read()
-	blacklist = _text_.split("\n")
+bot.load_extension("main")
 
 
 @bot.command()
-async def invite(ctx):
-	link = f"https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions=8&scope=bot"
-	embed = discord.Embed(description=f":small_blue_diamond: Add me to your server: [[Invite]]({link})")
-	await ctx.send(embed=embed)
+@commands.has_permissions(manage_guild=True)
+@commands.cooldown(1, 30, commands.BucketType.guild)
+async def prefix(ctx, prefix):
+	if len(prefix) > 5:
+		return await fail(ctx, "Prefix cannot be longer than 5 charters.")
+	if "`" in prefix:
+		return await fail(ctx, "Prefix contains illegal charters.")
+	key = ctx.guild.id
+	set_prefix(key, prefix)
+	await done(ctx, f"Command prefix for this server has been changed to `{prefix}`.")
 
 
 @bot.event
 async def on_ready():
 	print("Logged in.")
-	presence = f'{bot.command_prefix}invite | [{len(bot.guilds)}]'
-	await bot.change_presence(status=discord.Status.dnd, activity=discord.Game(presence))
+	await presence_loop(bot)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+	msg = None
+	if isinstance(error, commands.errors.CommandInvokeError):
+		_error = str(error).replace("Command raised an exception: ", "")
+		msg = f"An error has occured.\n```{_error}```"
+	if isinstance(error, commands.errors.CommandNotFound):
+		return
+	if isinstance(error, commands.CommandOnCooldown):
+		msg = f"You will able to use this command in {int(error.retry_after)} seconds."
+	if isinstance(error, commands.errors.BotMissingPermissions):
+		msg = "I don\'t have permissions to execute this command."
+	if isinstance(error, commands.errors.MissingRequiredArgument):
+		msg = "Missing required argument."
+	if isinstance(error, commands.MissingPermissions):
+		msg = f'You don\'t have permission to use this command.'
+	return await fail(ctx, msg)
 
 
 @bot.event
 async def on_message(message):
 	if message.author.bot:
 		return
+	if message.content == f"<@!{bot.user.id}>":
+		p = get_prefix(message.guild.id)
+		await message.reply(f"{vmark} Мой префикс: [`{p}`].")
 	await bot.process_commands(message)
-	await scan_message(message)
-
-
-async def scan_message(message):
-	index = 0
-	for elem in blacklist:
-		if elem in message.content.lower() and elem != "":
-			return await delete(message, index, 0, 0, "message.content")
-		index += 1
-	index = 0
-	for elem in patterns_blacklist:
-		if re.findall(elem, message.content.lower().replace("\n", " ")):
-			return await delete(message, index, 0, 2, "message.content")
-		index += 1
-	if not message.embeds and "http" in message.content:
-		await asyncio.sleep(1)
-		message = await message.channel.fetch_message(message.id)
-	index = 0
-	for embed in message.embeds:
-		if await check_embed(embed, message, index):
-			return
-		index += 1
-
-
-async def check_embed(embed, message, index):
-	indexx = 0
-	for elem in embed_blacklist:
-		try:
-			if elem in embed.title.lower() and elem != "":
-				return await delete(message, index, indexx, 1, "title")
-		except:
-			return False
-		try:
-			if elem in embed.description.lower() and elem != "":
-				return await delete(message, index, indexx, 1, "description")
-		except:
-			return False
-		indexx += 1
-
-
-async def delete(message, index, indexx, rindex, blkey):
-	await message.delete()
-	reason = reasons[rindex].format(f"{blkey}: {[indexx]}: {index}")
-	embed = discord.Embed(description=f":warning: Deleted message from user {message.author.mention}.\n \➡ **Reason**: **`{reason}`**.")
-	await message.channel.send(embed=embed)
-	embed = discord.Embed(description=f":warning: **Your message has been deleted**.\n```{message.content}```")
-	embed.set_footer(text="Most likely, you have become a victim of hacking and your account has been used for scam attacks. To prevent this from happening again, please change your password, delete BetterDiscord from your PC and use a reliable antivirus.")
-	await message.author.send(embed=embed)
+	db = db_read()
+	key = str(message.guild.id)
+	key_ = message.guild.id
+	dm = key_ not in db["nodms"]
+	notify = key_ not in db["dontnotify"]
+	disabled = db.get("disabled", [])
+	cid = db["logchannels"].get(key)
+	if key_ not in disabled:
+		await scan_message(message, notify=notify, cid=cid, dm=dm)
 
 
 bot.run(Token)
