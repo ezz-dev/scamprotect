dc7fff013e1d63d5d6c019d10747127f70f1b72d
xshadowsexy <82493057+Sweety187@users.noreply.github.com>
2021-09-23 22:27:18 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ bot = commands.Bot(command_prefix=determine_prefix,
 		   case_insensitive=True,
 		   help_command=None)
 Token = "TOKEN"
+
+print("[Main Thread] Loading Command Listener...")
 bot.load_extension("main")
 
 
@@ -15,17 +17,17 @@ bot.load_extension("main")
 @commands.cooldown(1, 30, commands.BucketType.guild)
 async def prefix(ctx, prefix):
 	if len(prefix) > 5:
-		return await fail(ctx, "Prefix cannot be longer than 5 charters.")
+		return await fail(ctx, "Префикс не может быть длиннее 5 символов.")
 	if "`" in prefix:
-		return await fail(ctx, "Prefix contains illegal charters.")
+		return await fail(ctx, "Префикс содержит недопустимый символ.")
 	key = ctx.guild.id
 	set_prefix(key, prefix)
-	await done(ctx, f"Command prefix for this server has been changed to `{prefix}`.")
+	await done(ctx, f"Префикс для этого сервера иземенен на `{prefix}`.")
 
 
 @bot.event
 async def on_ready():
-	print("Logged in.")
+	print("[Main Thread] Logged in.")
 	await presence_loop(bot)
 
 
@@ -34,37 +36,43 @@ async def on_command_error(ctx, error):
 	msg = error
 	if isinstance(error, commands.errors.CommandInvokeError):
 		_error = str(error).replace("Command raised an exception: ", "")
-		msg = f"An error has occured.\n```{_error}```"
+		print(f"[Main Thread] Command `{ctx.message.content}` raised an exception: `{_error}`")
+		msg = f"An error has occured.\n```py\n{_error}```"
 	if isinstance(error, commands.errors.CommandNotFound):
 		return
 	if isinstance(error, commands.CommandOnCooldown):
-		msg = f"You will able to use this command in {int(error.retry_after)} seconds."
+		msg = f"This command will be available in {int(error.retry_after)} seconds."
 	if isinstance(error, commands.errors.BotMissingPermissions):
-		msg = "I don\'t have permissions to execute this command."
+		msg = "I don't have permission to execute this command."
 	if isinstance(error, commands.errors.MissingRequiredArgument):
 		msg = "Missing required argument."
 	if isinstance(error, commands.MissingPermissions):
-		msg = f'You don\'t have permission to use this command.'
-	return await fail(ctx, msg)
+		msg = f"You are missing permission to invoke this command."
+	if isinstance(error, commands.errors.ChannelNotFound):
+		msg = "Channel not found."
+	if isinstance(error, commands.BadArgument):
+		msg = "Bad argument."
+	if isinstance(error, commands.errors.NotOwner):
+		msg = "You can't invoke this command."
+	await fail(ctx, msg)
 
 
 @bot.event
 async def on_message(message):
 	if message.author.bot:
 		return
-	if message.content == f"<@{bot.user.id}>":
+	if message.content == f"<@!{bot.user.id}>":
 		p = get_prefix(message.guild.id)
-		await message.reply(f"{vmark} Мой префикс: [`{p}`].")
+		return await message.reply(f"{vmark} My prefix: [`{p}`].")
+
 	await bot.process_commands(message)
-	db = db_read()
-	key = str(message.guild.id)
-	key_ = message.guild.id
-	dm = key_ not in db["nodms"]
-	notify = key_ not in db["dontnotify"]
-	disabled = db.get("disabled", [])
-	cid = db["logchannels"].get(key)
-	if key_ not in disabled:
+
+	key = message.guild.id
+	dm, notify, disabled, cid = fetch_scanner_arguments(key)
+
+	if key not in disabled:
 		await scan_message(message, notify=notify, cid=cid, dm=dm)
 
 
+print("[Main Thread] Logging in...")
 bot.run(Token)
